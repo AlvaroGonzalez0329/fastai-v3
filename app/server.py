bcd52519c0fe3f8f16373fcc9bc5f8df5313dae2
AlvaroGonzalez0329 <64087959+AlvaroGonzalez0329@users.noreply.github.com>
2020-04-28 01:13:38 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1X9bxZBZ8HhaPAN0CF1WsNSXLLKnph6Cw'
 export_file_name = 'xray_covi19.pkl'
 
-classes = ['apple', 'cherry', 'strawberry']
+classes = ['', 'ARDS', 'COVID19', 'Normal', 'Pnemonia', 'SARS', 'Streptococcus', 'StressSmoking', 'Virus', 'bacteria']
 path = Path(__file__).parent
 
 app = Starlette()
